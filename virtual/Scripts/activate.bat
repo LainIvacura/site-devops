b175d43adcb99020d4da6e0f7a39c7679e609481
LainIvacura <marmotdays@gmail.com>
2024-05-12 14:52:05 +0300

    устанолена библиотека pillow

--- virtual/Scripts/activate.bat
+++ virtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\2 PC prof\site devops\virtual
+set VIRTUAL_ENV=D:\koko\virtual
 
 if not defined PROMPT set PROMPT=$P$G
 
